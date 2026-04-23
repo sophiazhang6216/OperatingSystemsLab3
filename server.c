d57c134fc56eb47e3034a1fd748ea816f6fa9ab8
Sophia Zhang <s.w.zhang@iht32-1501.engr.wustl.edu>
2026-04-23 11:10:38 -0500

    step 2 almost done

---
+++ server.c
@@ -0,0 +1,67 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct src_node{
+    FILE * ptr;
+
+} src_node;
+
+int main(int argc, char *argv[]){
+
+    const char * file_name = argv[1];
+    const int port_num = atoi(argv[2])
+    FILE * src_file;
+    FILE * dst_file;
+    char * line;
+    size_t size = 0;
+    ssize_t nread;
+    int line_count = 0;
+    src_node * nodes;
+    int i = 0;
+
+    if (argc > 3){
+        printf("wrong number of cmd line args\n");
+        return -1;
+    }
+
+    src_file = fopen(file_name, "r");
+    if (src_file == NULL){
+        printf("fopen failed \n");
+        return -1;
+    }
+
+    while (nread = getline(&line, &size, src_file) != -1){
+        line_count++;
+    }
+
+    fclose(src_file);
+
+    nodes = (src_node *)malloc(sizeof(src_node)*(line_count-1));
+
+    src_file = fopen(file_name, "r");
+    if (src_file == NULL){
+        printf("fopen failed \n");
+        return -1;
+    }
+
+    while (nread = getline(&line, &size, src_file) != -1){
+        if (i == 0){
+            dst_file = fopen(line, "w");
+            if (src_file == NULL){
+                printf("fopen dest file failed \n");
+                return -2;
+            }
+        }
+        else{
+            node[i].ptr = fopen(line, "r");
+            if (node[i].ptr == NULL){
+                printf("fopen input snippet file failed \n");
+                return -3;
+            }
+        }
+        i++;
+    }
+
+
+}
