b9a1c0fd381ce510366c5739532c14c0e7d815cb
a-burba <a.burba@wustl.edu>
2026-04-25 11:20:04 -0500

    fixed segfaults - can socket

--- server.c
+++ server.c
@@ -12,19 +12,20 @@
 #include <netdb.h>
 #include <sys/epoll.h>
 #include "rbtree.h"
+#include <ctype.h> // isspace
 
 #define LISTEN_BACKLOG 50
 #define MAX_EVENTS     10
 #define BUF_SIZE 2048
 #define TRUE 1
 #define FALSE 0
+#define EXPECTED_ARGS 3
 
 enum exit_values {
     SUCCESS = 0,
-    WRONG_NUM_ARGS,
-    OPEN_SRC_FILE_FAILED,
-    OPEN_DESTINATION_FILE_FAILED,
-    OPEN_INPUT_SNIPPET_FILE_FAILED
+    WRONG_NUM_ARGS
+    // other return values are the errno for the failure reason -
+    // specific function that caused the error is printed to stdout
 };
 
 typedef struct src_node {
@@ -38,7 +39,7 @@ typedef struct tree_node {
 } tree_node;
 
 int handle_error(char* msg) {
-    printf("Program error in %s, reason: %s\n", msg, strerror(errno));
+    printf("Program error in %s. Reason: %s\n", msg, strerror(errno));
     return errno;
 }
 
@@ -47,6 +48,8 @@ int count_lines_in_file(const char * src_file_name, int* line_count) {
     ssize_t nread;
     char * line;
     size_t size = 0;
+
+    *line_count = 0;
 
     src_file = fopen(src_file_name, "r");
     if (src_file == NULL){
@@ -62,52 +65,65 @@ int count_lines_in_file(const char * src_file_name, int* line_count) {
     return SUCCESS;
 }
 
-int open_fragment_files(const char * src_file_name, src_node ** nodes, int** connect_fd) {
+// TODO trim all whitespace? Or start trimming from back in case preceding whitespace
+void trim_trailing_whitespace(char* line) {
+    int i;
+    for (i = 0; i < strlen(line); ++i) {
+        // characters covered by isspace(): ' ', '\f', '\n', '\r', '\t', '\v'
+        if (isspace(line[i])) {
+            line[i] = '\0';
+            return;
+        }
+    }
+}
+
+int open_fragment_files(const char * src_file_name, src_node ** nodes, int** connect_fds, int* num_fragment_files) {
     FILE * src_file;
     FILE * dst_file;
     ssize_t nread;
+    size_t size;
     char * line;
-    size_t size = 0;
-    int i, ret;
-    int line_count = 0;
+    int line_count, i, ret;
 
     ret = count_lines_in_file(src_file_name, &line_count);
     if (ret != SUCCESS) {
+        // count_lines_in_file prints out the specific error message
         return ret;
     }
+
+    *num_fragment_files = line_count - 1; // don't need to store first line (dest file name)
     
-    connect_fd = malloc(sizeof(int) * (line_count-1)); // don't need to store first line (dest file name)
-    if (connect_fd == NULL) {
-        return handle_error("malloc connect_fd");
+    *connect_fds = malloc(sizeof(int) * (*num_fragment_files)); 
+    if (*connect_fds == NULL) {
+        return handle_error("malloc connect_fds");
     }
 
-    printf("got through malloc 1\n");
-
-    *nodes = (src_node *)malloc(sizeof(src_node)*(line_count-1));
+    *nodes = (src_node *)malloc(sizeof(src_node)*(*num_fragment_files));
     if (*nodes == NULL) {
         return handle_error("malloc nodes");
     }
-
-    printf("got through malloc 2\n");
     
     src_file = fopen(src_file_name, "r");
     if (src_file == NULL){
         return handle_error("fopen source file");
     }
 
+    i = 0;
     while (nread = getline(&line, &size, src_file) != -1){
+        trim_trailing_whitespace(line);
+
         if (i == 0){
             dst_file = fopen(line, "w");
             if (src_file == NULL){
-                printf("fopen dest file failed \n");
-                return OPEN_DESTINATION_FILE_FAILED;
+                printf("destination file name: %s\n", line);
+                return handle_error("fopen destination file");
             }
         }
         else{
             (*nodes)[i].ptr = fopen(line, "r");
             if ((*nodes)[i].ptr == NULL){
-                printf("fopen input snippet file failed \n");
-                return OPEN_INPUT_SNIPPET_FILE_FAILED;
+                printf("issue with %s's line %d file name: %s\n", src_file_name, i, line);
+                return handle_error("fopen input snippet file");
             }
         }
         i++;
@@ -175,46 +191,40 @@ struct tree_node *tree_get_and_remove(struct rb_root *root, char *string) //code
 
 int main(int argc, char *argv[]){
     char * file_name;
-    int port_num;
-    struct epoll_event events[MAX_EVENTS];
-    int* connect_fd;
-    FILE * src_file;
-    FILE * dst_file;
-    char * line;
+    int port_num, num_fragment_files;
     struct sockaddr_in my_addr, peer_addr;
     socklen_t peer_addr_size;
-    size_t size = 0;
-    ssize_t nread;
-    int epfd, sfd;
-    int finished_clients;
-    int line_count = 0;
+    int epfd, sfd, finished_clients;
+    struct epoll_event events[MAX_EVENTS];
+    int * connect_fds;
     src_node * nodes;
-    int i = 0;
-    int n;
-    int ret;
-    finished_clients = 0;
+    int i, j, n, ret, fd;
+    uint32_t ev_mask;
     struct rb_root mytree;
+
     mytree = RB_ROOT;
 
-    if (argc != 3){
-        printf("wrong number of cmd line args\n");
+    if (argc != EXPECTED_ARGS){
         printf("Usage: %s <input_file_name> <port number>\n", argv[0]);
+        printf("    input file should be formatted as follows:\n");
+        printf("    first line: output file name\n");
+        printf("    subsequent lines: file names of input fragments (one per line)\n");
+        printf("    note: file names should not contain spaces\n");
         return WRONG_NUM_ARGS;
     }
 
     file_name = argv[1];
     port_num = atoi(argv[2]);
 
-    ret = open_fragment_files(file_name, &nodes, &connect_fd);
+    ret = open_fragment_files(file_name, &nodes, &connect_fds, &num_fragment_files);
     if (ret != SUCCESS) {
-        // message for specific error reason is printed out within function
-        return ret;
+        return ret; // message for specific error reason is printed out within function
     }
 
     printf("about to socket\n");
 
     sfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sfd == -1) return handle_error("socket");
+    if (sfd == -1) return handle_error("socket()");
 
     memset(&my_addr, 0, sizeof(my_addr));
     my_addr.sin_family = AF_INET;
@@ -222,28 +232,29 @@ int main(int argc, char *argv[]){
     my_addr.sin_addr.s_addr = INADDR_ANY;
 
     if (bind(sfd, (struct sockaddr *)&my_addr, sizeof(my_addr)) == -1)
-        return handle_error("bind");
+        return handle_error("bind()");
     if (listen(sfd, LISTEN_BACKLOG) == -1)
-        return handle_error("listen");
+        return handle_error("listen()");
 
     epfd = epoll_create1(0);
-    if (epfd == -1) return handle_error("epoll_create1");
+    if (epfd == -1) return handle_error("epoll_create1()");
 
+    finished_clients = 0;
     for(;;){
         n = epoll_wait(epfd, events, MAX_EVENTS, -1);
         if (n == -1) {
-            return handle_error("epoll_wait");
+            return handle_error("epoll_wait()");
         }
-        for (int i = 0; i < n; i++) { //bc epoll can see multiple events ready at the same time gotta loop
-            int fd = events[i].data.fd;
-            uint32_t ev_mask = events[i].events;
+        for (i = 0; i < n; i++) { //bc epoll can see multiple events ready at the same time gotta loop
+            fd = events[i].data.fd;
+            ev_mask = events[i].events;
             if(fd == sfd){
                 //accept the connection
                 //send them a chunk
                 //do the bookkeeping
             } else{
-                for(int j = 0; j < MAX_EVENTS; j++){
-                    if(fd == connect_fd[j]){
+                for(j = 0; j < MAX_EVENTS; j++){
+                    if(fd == connect_fds[j]){
                         if (ev_mask & EPOLLIN) {
                             //read in their desc
                             //if full read then add it to the rb tree
@@ -256,7 +267,7 @@ int main(int argc, char *argv[]){
                 }
             }
         }
-        if(finished_clients == line_count-1){
+        if(finished_clients == num_fragment_files){
             //all of our chunks have been processed
             break;
         }
